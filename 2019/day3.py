e8d671b9438bee1f3600c6b6b72c7284664b4683
alex.brewster <alexb@ca.ibm.com>
2019-12-05 13:41:07 -0400

    day3 set up

---
+++ 2019/day3.py
@@ -0,0 +1,23 @@
+import sys
+
+# find the intersection point closest to the central port
+
+def part_one(point):
+
+    return point
+
+
+
+def part_two(point):
+ 
+    return point
+    
+
+    
+        
+if __name__ == '__main__':
+    data = [int(i) for i in input().split(',')]
+    print (f"THIS IS NUBER ONE {part_one(data)}")
+    print (f"THIS IS NUMBER TWO {part_two(data)}")
+    
+
